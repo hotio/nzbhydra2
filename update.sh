261f809a5fb936ec33529069342573d47e259f42
hotio <hotio@protonmail.com>
2019-11-16 10:03:36 +0100

    [CI SKIP] add failsafe

--- update.sh
+++ update.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 version=$(curl -fsSL "https://api.github.com/repos/theotherp/nzbhydra2/releases/latest" | jq -r .tag_name | sed s/v//g)
+[[ -z ${version} ]] && exit
 find . -type f -name '*.Dockerfile' -exec sed -i "s/ARG NZBHYDRA2_VERSION=.*$/ARG NZBHYDRA2_VERSION=${version}/g" {} \;
 sed -i "s/{TAG_VERSION=.*}$/{TAG_VERSION=${version}}/g" .drone.yml
 echo "##[set-output name=version;]${version}"
